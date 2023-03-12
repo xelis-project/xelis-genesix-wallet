7dde240dd37ba8d757c1471ea22421281b350cfe
Ez3kiel <ez3kiel.2020@protonmail.com>
2023-03-12 18:30:50 +0100

    chore: misc

--- rust/src/api.rs
+++ rust/src/api.rs
@@ -1,85 +1,11 @@
-// use anyhow::Error;
-// use flutter_rust_bridge::*;
-// pub use std::panic::{RefUnwindSafe, UnwindSafe};
-// use std::sync::{Arc, Mutex, RwLock};
-// use xelis_wallet::network_handler::SharedNetworkHandler;
-// use xelis_wallet::storage::EncryptedStorage;
-// pub use xelis_wallet::wallet::Wallet;
-// // pub use xelis_common::crypto::key::{KeyPair, PrivateKey, PublicKey};
-//
-// /*#[frb(mirror(Wallet))]
-// pub struct _Wallet {
-//     // Encrypted Wallet Storage
-//     storage: RwLock<EncryptedStorage>,
-//     // Private & Public key linked for this wallet
-//     keypair: KeyPair,
-//     // network handler for online mode to keep wallet synced
-//     network_handler: Mutex<Option<SharedNetworkHandler>>,
-//     // network: Network
-// }*/
-// /*
-// pub struct XelisWallet {
-//     pub wallet: Arc<Wallet>,
-// }
-//
-// impl XelisWallet {
-//     pub fn create(name: String,
-//                   password: String,
-//                   seed: Option<String>, ) -> Self {
-//         Self {
-//             wallet: Wallet::create(name, password, seed).unwrap()
-//         }
-//     }
-// }*/
-//
-// /*pub fn new_wallet(name: String, password: String, seed: Option<String>) -> RustOpaque<Arc<Wallet>> {
-//     let wallet = Wallet::create(name, password, seed).unwrap();
-//     RustOpaque::new(wallet)
-// }*/
-// pub fn get_address(wallet: RustOpaque<Arc<Wallet>>) -> Result<String, Error> {
-//     Ok(wallet.lock().unwrap().get_address().as_string()?)
-// }
+use anyhow::{Error, Result};
+use flutter_rust_bridge::*;
+pub use xelis_common::crypto::key::{KeyPair, PrivateKey, PublicKey};
 
-/*pub fn create_key_pair() -> RustOpaque<KeyPair> {
+pub fn create_key_pair() -> RustOpaque<KeyPair> {
     RustOpaque::new(KeyPair::new())
 }
 
-pub fn get_address(key_pair: RustOpaque<KeyPair>) -> Result<String, anyhow::Error> {
+pub fn get_address(key_pair: RustOpaque<KeyPair>) -> Result<String, Error> {
     Ok(key_pair.get_public_key().to_address().as_string()?)
-}
-*/
-
-/*pub enum Platform {
-    Unknown,
-    Android,
-    Ios,
-    Windows,
-    Unix,
-    MacIntel,
-    MacApple,
-    Wasm,
-}
-
-pub fn platform() -> Platform {
-    if cfg!(windows) {
-        Platform::Windows
-    } else if cfg!(target_os = "android") {
-        Platform::Android
-    } else if cfg!(target_os = "ios") {
-        Platform::Ios
-    } else if cfg!(all(target_os = "macos", target_arch = "aarch64")) {
-        Platform::MacApple
-    } else if cfg!(target_os = "macos") {
-        Platform::MacIntel
-    } else if cfg!(target_family = "wasm") {
-        Platform::Wasm
-    } else if cfg!(unix) {
-        Platform::Unix
-    } else {
-        Platform::Unknown
-    }
-}*/
-
-pub fn rust_release_mode() -> bool {
-    cfg!(not(debug_assertions))
 }
--- rust/src/bridge_generated.io.rs
+++ rust/src/bridge_generated.io.rs
@@ -2,17 +2,53 @@ use super::*;
 // Section: wire functions
 
 #[no_mangle]
-pub extern "C" fn wire_rust_release_mode(port_: i64) {
-    wire_rust_release_mode_impl(port_)
+pub extern "C" fn wire_create_key_pair(port_: i64) {
+    wire_create_key_pair_impl(port_)
+}
+
+#[no_mangle]
+pub extern "C" fn wire_get_address(port_: i64, key_pair: wire_KeyPair) {
+    wire_get_address_impl(port_, key_pair)
 }
 
 // Section: allocate functions
 
+#[no_mangle]
+pub extern "C" fn new_KeyPair() -> wire_KeyPair {
+    wire_KeyPair::new_with_null_ptr()
+}
+
 // Section: related functions
+
+#[no_mangle]
+pub extern "C" fn drop_opaque_KeyPair(ptr: *const c_void) {
+    unsafe {
+        Arc::<KeyPair>::decrement_strong_count(ptr as _);
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn share_opaque_KeyPair(ptr: *const c_void) -> *const c_void {
+    unsafe {
+        Arc::<KeyPair>::increment_strong_count(ptr as _);
+        ptr
+    }
+}
 
 // Section: impl Wire2Api
 
+impl Wire2Api<RustOpaque<KeyPair>> for wire_KeyPair {
+    fn wire2api(self) -> RustOpaque<KeyPair> {
+        unsafe { support::opaque_from_dart(self.ptr as _) }
+    }
+}
 // Section: wire structs
+
+#[repr(C)]
+#[derive(Clone)]
+pub struct wire_KeyPair {
+    ptr: *const core::ffi::c_void,
+}
 
 // Section: impl NewWithNullPtr
 
@@ -26,6 +62,13 @@ impl<T> NewWithNullPtr for *mut T {
     }
 }
 
+impl NewWithNullPtr for wire_KeyPair {
+    fn new_with_null_ptr() -> Self {
+        Self {
+            ptr: core::ptr::null(),
+        }
+    }
+}
 // Section: sync execution mode utility
 
 #[no_mangle]
