b0ddf4a75f11f3b51323efd8b0b2a11abc4615c9
Ez3kiel <ez3kiel.2020@protonmail.com>
2025-01-03 13:05:17 +0100

    fix(windows): TextField does not accept any input after biometric auth

--- lib/features/authentication/data/biometric_auth_repository.dart
+++ lib/features/authentication/data/biometric_auth_repository.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:local_auth/local_auth.dart';
+import 'package:window_manager/window_manager.dart';
 
 class BiometricAuthRepository {
   final LocalAuthentication auth = LocalAuthentication();
@@ -13,12 +14,23 @@ class BiometricAuthRepository {
   }
 
   Future<bool> authenticate(String reason) async {
-    return await auth.authenticate(
+    final res = await auth.authenticate(
       localizedReason: reason,
       options: AuthenticationOptions(
         biometricOnly: Platform.isWindows ? false : true,
       ),
     );
+
+    // this is needed to fix issue with local_auth on Windows
+    // see https://github.com/flutter/flutter/issues/122322
+    if (Platform.isWindows) {
+      await windowManager.setAlwaysOnTop(true);
+      await windowManager.blur();
+      await windowManager.show();
+      await windowManager.setAlwaysOnTop(false);
+    }
+
+    return res;
   }
 
   Future<void> stopAuthentication() async {
