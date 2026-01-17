eebca87a9d779a32d0954208d0c44d899341943e
Tritonn204 <tritonn204@gmail.com>
2026-01-16 17:09:49 -0800

    misc: l1 as custom(24) instead of l1Medium on non-desktop

--- lib/features/authentication/application/authentication_service.dart
+++ lib/features/authentication/application/authentication_service.dart
@@ -499,7 +499,7 @@ class Authentication extends _$Authentication {
     if (isDesktopDevice) {
       return PrecomputedTableType.l1Full();
     } else {
-      return PrecomputedTableType.l1Medium();
+      return PrecomputedTableType.custom(BigInt.from(24));
     }
   }
 }
