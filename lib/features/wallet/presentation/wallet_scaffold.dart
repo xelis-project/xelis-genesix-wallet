95ad3002e4bf144c82a61fd6bf7c61de129c8d96
Ez3kiel <Ez3kiel.2020@protonmail.com>
2026-02-12 22:10:32 +0100

    feat: implement pop scope to better manage the back button on Android

--- lib/features/wallet/presentation/wallet_scaffold.dart
+++ lib/features/wallet/presentation/wallet_scaffold.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:forui/forui.dart';
+import 'package:genesix/features/router/route_utils.dart';
 import 'package:genesix/features/wallet/presentation/side_bar/side_bar.dart';
 import 'package:genesix/shared/theme/constants.dart';
 import 'package:genesix/shared/theme/build_context_extensions.dart';
@@ -27,47 +28,51 @@ class WalletScaffold extends ConsumerStatefulWidget {
 
 class _WalletScaffoldState extends ConsumerState<WalletScaffold> {
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (context.mounted && context.canPop()) {
-        context.pop();
-      }
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     final needTitle =
         widget.title != null && widget.title!.isNotEmpty && context.isMobile;
 
-    return FScaffold(
-      header: FHeader.nested(
-        title: needTitle
-            ? Padding(
-                padding: const EdgeInsets.symmetric(vertical: Spaces.small),
-                child: Text(widget.title!),
-              )
-            : SizedBox.shrink(),
-        prefixes: context.isMobile
-            ? [
-                Padding(
-                  padding: const EdgeInsets.all(Spaces.small),
-                  child: FHeaderAction(
-                    icon: const Icon(FIcons.menu),
-                    onPress: () => showFSheet<void>(
-                      context: context,
-                      side: FLayout.ltr,
-                      builder: (context) => SideBar(widget.goRouterState),
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (didPop, _) {
+        if (didPop) return;
+
+        // If already on home, do nothing (don't exit the app)
+        if (context.goRouterState.fullPath == AuthAppScreen.home.toPath) {
+          return;
+        }
+
+        // Otherwise, go back to home
+        context.go(AuthAppScreen.home.toPath);
+      },
+      child: FScaffold(
+        header: FHeader.nested(
+          title: needTitle
+              ? Padding(
+                  padding: const EdgeInsets.symmetric(vertical: Spaces.small),
+                  child: Text(widget.title!),
+                )
+              : SizedBox.shrink(),
+          prefixes: context.isMobile
+              ? [
+                  Padding(
+                    padding: const EdgeInsets.all(Spaces.small),
+                    child: FHeaderAction(
+                      icon: const Icon(FIcons.menu),
+                      onPress: () => showFSheet<void>(
+                        context: context,
+                        side: FLayout.ltr,
+                        builder: (context) => SideBar(widget.goRouterState),
+                      ),
                     ),
                   ),
-                ),
-              ]
-            : [],
-        suffixes: widget.headerSuffixes ?? [],
+                ]
+              : [],
+          suffixes: widget.headerSuffixes ?? [],
+        ),
+        sidebar: !context.isMobile ? SideBar(widget.goRouterState) : null,
+        child: BodyLayoutBuilder(child: widget.child),
       ),
-      sidebar: !context.isMobile ? SideBar(widget.goRouterState) : null,
-      child: BodyLayoutBuilder(child: widget.child),
     );
   }
 }
